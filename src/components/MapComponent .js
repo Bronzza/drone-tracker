17915556da882a4e5cd03bd6247bf26a55723509
Serhii Repinetskyi <repinetskiy@gmail.com>
2024-09-27 17:34:52 +0700

    Add drone icon

--- src/components/MapComponent .js
+++ src/components/MapComponent .js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'; 
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
+import planeImg from '../img/aircraft-jet-icon.svg';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 const deviceId = process.env.REACT_APP_DEVICE_ID;
 const apiUrl = `${baseUrl}/${deviceId}`;
+const defaultLatt = 50.4501;
+const defaultLong = 30.5234;
 
-console.log(apiUrl);
+const planeIcon = new L.Icon({
+  iconUrl: planeImg, // Make sure to replace this with the actual path
+  iconSize: [50, 50], // Size of the icon
+  iconAnchor: [25, 50], // Point of the icon which will correspond to marker's location
+  popupAnchor: [0, -50] // Point from which the popup should open relative to the iconAnchor
+});
 
 const MapComponent = () => {
-  const [position, setPosition] = useState([0, 0]);
+  const [position, setPosition] = useState([defaultLatt, defaultLong]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -42,7 +51,7 @@ const MapComponent = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+      <Marker position={position} icon={planeIcon}>
         <Popup>
           Drone Location: <br /> Latitude: {position[0]} <br /> Longitude: {position[1]}
         </Popup>
